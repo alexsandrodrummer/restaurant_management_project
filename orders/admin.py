3a54a952494b9d431c9ac0244cee3a11d2314a1a
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-09-26 16:39:43 +0100

    50449

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, OrderStatus
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['code', 'total','created_at']
+
+@admin.register(Order)
+class OrderStatusAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
