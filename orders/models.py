b22490b77d450b1bce302eae15795675ba8377a3
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-09-26 16:26:32 +0100

    50447

--- orders/models.py
+++ orders/models.py
@@ -5,5 +5,17 @@ class Order(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
         return self.code
+
+class OrderStatus (models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = "Order status"
+        verbose_name_plural = "Order statuses"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
