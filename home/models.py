41cb8a46dde0fbc20458b1db4a76e576ac38802c
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-09-26 17:16:14 +0100

    50445

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MenuCategory(models.Model):
+    name = models.charField(max_length=100, inique=True)
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "Menu category"
+        verbose_name_plural = "Menu categories"
+
+    def __str__(self):
+        return self.name
