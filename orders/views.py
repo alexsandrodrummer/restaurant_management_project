b7b25e4ef53e5efda6e6700b48b980900578c427
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-09-29 16:42:59 +0100

    51793

--- orders/views.py
+++ orders/views.py
@@ -18,7 +18,25 @@ class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+class OrderHistoryview(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend, django_filters.SearchFilter, drf_filters.OrderingFilter]
+    filterset_class = OrderFilter
+    search_fields = ["code"]
+    ordering_fields = ["created_at", "total"]
+    ordering = ["-created_at"]
 
+
+    def get_queryset(self):
+
+        return(
+            Order.objects
+            .filter(user=self.request.user)
+            .select_related("status", "user")
+            .prefetch_related("items")
+            .order_by("-created_at")
+        )
     # opcional: você pode omitir se já definiu no settings
 #     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
 #     filterset_class = OrderFilter
