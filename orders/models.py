a6d9ab286809bdc7fb2ec4c14cb520bc74b56b76
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-09-29 16:20:48 +0100

    51793

--- orders/models.py
+++ orders/models.py
@@ -30,3 +30,13 @@ class Order(models.Model):
 
     def __str__(self):
         return self.code
+
+
+class OrderItem(models.Model):
+    order = models.Foreignkey(Order, on_delete=models.CASCADE, related_name="items")
+    product_name = models.charField(max_length=150)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def__str__(self):
+        return f"{self.product_name} x{self.quantity}"
