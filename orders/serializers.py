e4725cefa126b1fa5c3bd1e28ffa5644007dc078
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-09-29 16:28:15 +0100

    51793

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from .models import Order
 
+class OrderItemSerializer(serializers.ModelSerializer):
+    line_total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = OrderItemSerializer
+        fields = ["id", "product_name", "unit_price", "quantity", "line_total"]
+
+    def get_line_total(self, obj):
+        return obj.unit_price * obj.quantity
+
+
 
 class OrderSerializer(serializers.ModelSerializer):
+
+    items = OrderItemSerializer(many=True,read_only=True)
+    status = serializers.charField(source="status.name", default=None, read_only=True)
+    date = serializers.DateTimeField(source="created_at", format="%y-%m-%d %H:%M:%S")
     class Meta:
         model = Order
         fields = ["id", "code", "total", "created_at"]
