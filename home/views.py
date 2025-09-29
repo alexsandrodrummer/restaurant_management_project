82e40d850c1815afaaf269d13eace796a6eeb9d5
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-09-29 17:27:01 +0100

    51792

--- home/views.py
+++ home/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from .serializers import MenuItemSerializer
 
 
-class MenuCategoryListView(ListAPIView):
-    queryset = MenuCategory.objects.all().order_by('name')
-    serializer_class = MenuCategorySerializer
-    permission_classes = []
-    authentication_classes = []
+class MenuItemsByCategoryView(ListAPIView):
+    serializer_class = MenuItemSerializer
+    def get_queryset(self):
+        qs = MenuItem.objects.filter(is_active=True).select_related('category')
+        category_id = self.request.query_params.get('category_id')
+        category_name = self.request.query_params.get('category')
+        if category_id:
+            return qs.filter(category_id=category_id)
+        if category_name:
+            return qs.filter(category__name__iexact=category_name)
+        return qs.none()
+    
