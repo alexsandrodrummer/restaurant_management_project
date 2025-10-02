c0caad37f6585a011bd70364b3989c4858afbf7e
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-10-02 15:26:41 +0100

    51789

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,8 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"items", MenuItemUpdateViewSet, basename='menu-item')
-router.register(r"items", MenuItemsByCategoryView, basename='menu-item')
+router.register(r"items", MenuItemsByCategoryView, basename='menu-item-category')
+router.register (r"items/search", MenuItemSearchViewset, basename="menu-item-search")
 
 urlpatterns =  router.urls
     
