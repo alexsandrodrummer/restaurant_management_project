d42a84475672ea5f42071c9621529cf02e01ac2c
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-10-02 14:45:28 +0100

    51790

--- home/serializer.py
+++ home/serializer.py
@@ -8,3 +8,21 @@ class MenuCategorySerializer(serializers.ModelSerializer):
         model = MenuItem
         fields = ['id', 'name', 'price', 'category', 'category_id', 'is_active']
         read_only_fields = ['category']
+
+
+class MenuItemUpdateSerializer (serializer.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ["id", "name", "description", "price", "is_active", "category"]
+        extra_kwargs = {
+            "name":{"required": False},
+            "description": {"required": False, "allow_blank": True},
+            "price": {"required":False},
+            "is_active":{"required": False },
+            "category": {"required": False},
+        }
+    def validate_price(self, value):
+        if value is not None and value <= 0:
+            raise serializers.ValidationError("price must be a positive number.")
+        
+        return value
