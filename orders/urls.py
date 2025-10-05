cd3722d9ac43fbd5643257e8ad1e76714f466558
Alex Sandro <79149058+alexsandrodrummer@users.noreply.github.com>
2025-10-05 14:52:11 +0100

    53181

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("orders", OrderListCreateView.as_view(), name="order-list-create"),
     path("orders/<int:pk>/", OrderDetailView.as_view(), name="order-detail"),
     path("orders/history/", OrderHistoryView.as_view(), name="order-history"),
+    path('coupons/validate/', CouponValidationView.as_view(), name='coupon-validate')
 ]
